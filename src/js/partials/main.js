df0c0b80400374e5e1341d0102667894bc3891e6
Anton Ivanov <ivanovanton.web@gmail.com>
2021-10-26 19:04:49 +0300

    added header & mobile menu

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
+window.addEventListener("DOMContentLoaded", function () {
+	const header = document.querySelector(".header");
 
-})
+	const toggleMenu = () => {
+		const nav = document.querySelector(".navigation");
+
+		nav.classList.toggle("is-active");
+	};
+
+	function clickHandler(e) {
+		const target = e.target;
+		const menuOpen = target.closest(".header__burger");
+		const menuClose = target.closest(".navigation__close");
+
+		if (menuOpen || menuClose) {
+			toggleMenu();
+		}
+	}
+
+	header.addEventListener("click", clickHandler);
+});
