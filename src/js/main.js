780c8de1051becfe7fc1c5766c1f1cb0aafe8d3c
Anton Ivanov <ivanovanton.web@gmail.com>
2021-10-28 11:47:44 +0300

    fixed masonry

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 //= ./../../node_modules/jquery/dist/jquery.min.js
 //= libs/masonry.pkgd.min.js
 //= libs/fancybox.js
+//= libs/fullpage.js
+//= libs/jquery.pagepiling.min.js
 
 //= ./partials/main.js
 //= ./partials/masonry.js
---
+++ src/js/partials/scroll.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+	function scrollBehavior() {
+		$(document).on("click", 'a[href^="#"]', function (event) {
+			event.preventDefault();
+
+			$("html, body").animate(
+				{
+					scrollTop: $($.attr(this, "href")).offset().top,
+				},
+				500
+			);
+		});
+	}
+
+	scrollBehavior();
+
+	$("#pagepiling").pagepiling({});
+});
