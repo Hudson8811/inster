1fc84a60636a264d1aac6860c616bad05fbe3e3d
Anton Ivanov <ivanovanton.web@gmail.com>
2021-11-01 18:39:27 +0300

    refact masonry

--- src/js/partials/masonry.js
+++ src/js/partials/masonry.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
-	$(".portfolio__list").masonry({
+	const $grid = $(".portfolio__list").masonry({
 		itemSelector: ".portfolio__item",
 		gutter: ".portfolio__gutter",
 	});
 
-	$(".portfolio__list")
-		.imagesLoaded()
-		.progress(function () {
-			$(".portfolio__list").masonry("layout");
-		});
+	$grid.imagesLoaded().progress(function () {
+		$grid.masonry("layout");
+	});
 });
