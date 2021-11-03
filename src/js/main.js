fa591a7a899bf302d8b20ff64fea31c78336374d
Anton Ivanov <ivanovanton.web@gmail.com>
2021-11-03 18:50:24 +0300

    refact

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 //= ./../../node_modules/jquery/dist/jquery.min.js
+//= libs/animsition.js
+//= libs/scrollreveal.min.js
 //= libs/imagesloaded.pkgd.min.js
 //= libs/masonry.pkgd.min.js
 //= libs/fancybox.js
 //= libs/jquery.pagepiling.min.js
-//= libs/scrollreveal.min.js
-//= libs/animsition.min.js
 
+//= ./partials/animation.js
+//= ./partials/scroll.js
 //= ./partials/main.js
 //= ./partials/masonry.js
-//= ./partials/scroll.js
 //= ./partials/validation.js
-//= ./partials/animation.js
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,12 @@
+//window.addEventListener("load", function () {
+//	window.__isLoaded = true;
+//	console.log("window.__isLoaded", window.__isLoaded);
+//});
+
+//window.addEventListener("pageshow", function () {
+//	window.__pageShowed = true;
+//});
+
 window.addEventListener("DOMContentLoaded", function () {
 	const header = document.querySelector(".header");
 	const portfolioItems = document.querySelectorAll(".portfolio__item");
@@ -24,20 +33,48 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	header.addEventListener("click", clickHandler);
 
-	const mouseHandler = (e) => {
-		const target = e.target;
-		const portfolio = target.closest(".portfolio");
-		const portfolioItem = target.closest(".portfolio__item");
-		const portfolio__list = target.matches(".portfolio__list");
+	let isDesktop = false;
 
-		if (portfolio__list || !portfolio) {
-			portfolioItems.forEach((item) => item.classList.remove("is-active"));
-		} else if (portfolioItem) {
-			portfolioItems.forEach((item) =>
-				item.classList[item === portfolioItem ? "add" : "remove"]("is-active")
-			);
+	const mobileHoverHandler = (query) => {
+		if (query.matches) {
+			if (isDesktop) document.removeEventListener("mouseover", mouseHandler);
+			const portfolioItems = document.querySelectorAll(".portfolio__item");
+
+			portfolioItems.forEach((item) => {
+				item.addEventListener("click", function () {
+					item.classList[
+						item.classList.contains("is-active") ? "remove" : "add"
+					]("is-active");
+				});
+			});
+		} else {
+			isDesktop = true;
+			const mouseHandler = (e) => {
+				const target = e.target;
+				const portfolio = target.closest(".portfolio");
+				const portfolioItem = target.closest(".portfolio__item");
+				const portfolio__list = target.matches(".portfolio__list");
+
+				if (portfolio__list || !portfolio) {
+					portfolioItems.forEach((item) => item.classList.remove("is-active"));
+				} else if (portfolioItem) {
+					portfolioItems.forEach((item) =>
+						item.classList[item === portfolioItem ? "add" : "remove"](
+							"is-active"
+						)
+					);
+				}
+			};
+
+			document.addEventListener("mouseover", mouseHandler);
 		}
 	};
 
-	document.addEventListener("mouseover", mouseHandler);
+	const checkMedia = () => {
+		const breakpoint = window.matchMedia("(max-width: 768px)");
+		mobileHoverHandler(breakpoint);
+		breakpoint.addEventListener("change", mobileHoverHandler);
+	};
+
+	checkMedia();
 });
