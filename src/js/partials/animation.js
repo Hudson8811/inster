70f4fb1d01b4ff94e72feefd260c3d678855940a
Anton Ivanov <ivanovanton.web@gmail.com>
2021-11-04 14:31:12 +0300

    refact

--- src/js/partials/animation.js
+++ src/js/partials/animation.js
@@ -3,22 +3,6 @@ $(".animsition").animsition({
 	outClass: "fade-out",
 	inDuration: 750,
 	outDuration: 400,
-	linkElement: ".animsition-link",
-	// e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-	loading: true,
-	loadingParentElement: "body", //animsition wrapper element
-	loadingClass: "animsition-loading",
-	loadingInner: "", // e.g '<img src="loading.svg" />'
-	timeout: false,
-	timeoutCountdown: 0,
 	onLoadEvent: true,
 	browser: ["animation-duration", "-webkit-animation-duration"],
-	// "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-	// The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-	overlay: false,
-	overlayClass: "animsition-overlay",
-	overlayParentElement: "body",
-	transition: function (url) {
-		window.location.href = url;
-	},
 });
